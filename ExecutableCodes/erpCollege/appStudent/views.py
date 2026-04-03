a1f12cd986cb1e83e592e2bd5758dd705943b40f
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-04-03 06:51:51 +0530

    initiated appStudent

---
+++ ExecutableCodes/erpCollege/appStudent/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+def myfunctioncall(request):
+    return HttpResponse("Hello World")
