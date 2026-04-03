ca4cd7840badabf0e7c026d67c5add65d564e52e
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-04-03 07:10:26 +0530

    Impl : To add the static and the templates sites and use it

--- ExecutableCodes/erpCollege/appStudent/views.py
+++ ExecutableCodes/erpCollege/appStudent/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def myfunctioncall(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, "login_student/index.html")
