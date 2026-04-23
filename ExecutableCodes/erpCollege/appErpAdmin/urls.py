fc5b75efca4f8a7894929d9c4859404f1828b7f3
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-04-23 15:15:17 +0530

    login using logic [demo in db.sqlite3]

--- ExecutableCodes/erpCollege/appErpAdmin/urls.py
+++ ExecutableCodes/erpCollege/appErpAdmin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.admin_home, name='admin_home'),
+    path('home/', views.admin_home, name='admin_home'),
+    path('', views.admin_login, name='admin_login'),
 ]
--- ExecutableCodes/erpCollege/appErpAdmin/views.py
+++ ExecutableCodes/erpCollege/appErpAdmin/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+import sqlite3
+from django.contrib.auth.hashers import check_password
 
 # Create your views here.
 
 
 def admin_home(request):
     return render(request, 'erpadmin/index.html')
+
+def admin_login(request):
+
+    if request.method == "POST":
+
+        empid = request.POST.get("empid")
+        password = request.POST.get("password")
+
+        conn = sqlite3.connect("db.sqlite3")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT passwordHash, status
+            FROM erp_admins
+            WHERE empid=?
+        """, (empid,))
+
+        row = cursor.fetchone()
+        conn.close()
+
+        if row:
+            stored_hash, status = row
+
+            if status == "active" and check_password(password, stored_hash):
+                return render(request, "erpadmin/success.html")
+
+        return render(request, "erpadmin/failure.html")
+
+    return render(request, "erpadmin/login.html")
