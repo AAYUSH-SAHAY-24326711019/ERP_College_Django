a0be205d5a7bbab038fcc24886a002f67c862bf6
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-05-06 18:18:25 +0530

    mainsite db connection initiated

--- ExecutableCodes/erpCollege/appMainsite/models.py
+++ ExecutableCodes/erpCollege/appMainsite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MainsiteEnquiryForm(models.Model):
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=13)
+    from_place=models.CharField(max_length=20)
+    course= models.CharField(max_length=20)
+
+class Meta:
+    db_table = "mainsite_enquiry_form"
--- ExecutableCodes/erpCollege/erpCollege/settings.py
+++ ExecutableCodes/erpCollege/erpCollege/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = "erpCollege.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erp_db',
+        'USER': 'admin_erp_db',
+        'PASSWORD': 'abcdef',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
