51955e53b2648e25db6bb559dfffd28607503739
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-06-07 16:20:18 +0530

    removed some old files

--- ExecutableCodes/erpCollege/jwt_notes/gen_stu_records.py
+++ ExecutableCodes/erpCollege/jwt_notes/gen_stu_records.py
@@ -12,18 +12,6 @@ INSERT INTO public."appStudent_student"
 '''
 
 for i in range (2,10):
-    print(f"('student{i}', 'stu00{i}', 'MCA',	2024-2026', 'students/images.jpg', NOW(), 'student{i}@erp.com'),")
+    print(f"('student{i}', 'stu00{i}', 'MCA',	2024-2026', null, NOW(), 'student{i}@erp.com'),")
 
-'''
-INSERT INTO public."appStudent_student"
-(id ,"name", student_id, course, "session", image, date_created, email)
-values
-(2,'student2', 'stu002', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student2@erp.com'),
-(3,'student3', 'stu003', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student3@erp.com'),
-(4,'student4', 'stu004', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student4@erp.com'),
-(5,'student5', 'stu005', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student5@erp.com'),
-(6,'student6', 'stu006', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student6@erp.com'),
-(7,'student7', 'stu007', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student7@erp.com'),
-(8,'student8', 'stu008', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student8@erp.com'),
-(9,'student9', 'stu009', 'MCA','2024-2026', 'students/images.jpg', NOW(), 'student9@erp.com')
-'''
+
