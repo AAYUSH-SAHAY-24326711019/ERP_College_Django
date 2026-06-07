672b409f2bd4d39ff60ba50fda5e3d0f39175e89
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-06-07 16:40:32 +0530

    adding sql file for all initial data

--- ExecutableCodes/erpCollege/jwt_notes/gen_stu_records.py
+++ ExecutableCodes/erpCollege/jwt_notes/gen_stu_records.py
@@ -11,7 +11,20 @@ INSERT INTO public."appStudent_student"
 ({i}, 'student{i}', 'stu00{i}', 'MCA',	2024-2026', 'students/images.jpg', NOW(), 'student{i}@erp.com')
 '''
 
-for i in range (2,10):
+for i in range (1,11):
     print(f"('student{i}', 'stu00{i}', 'MCA',	2024-2026', null, NOW(), 'student{i}@erp.com'),")
 
+'''
+('student1', 'stu001', 'MCA',   2024-2026', null, NOW(), 'student1@erp.com'),
+('student2', 'stu002', 'MCA',   2024-2026', null, NOW(), 'student2@erp.com'),
+('student3', 'stu003', 'MCA',   2024-2026', null, NOW(), 'student3@erp.com'),
+('student4', 'stu004', 'MCA',   2024-2026', null, NOW(), 'student4@erp.com'),
+('student5', 'stu005', 'MCA',   2024-2026', null, NOW(), 'student5@erp.com'),
+('student6', 'stu006', 'MCA',   2024-2026', null, NOW(), 'student6@erp.com'),
+('student7', 'stu007', 'MCA',   2024-2026', null, NOW(), 'student7@erp.com'),
+('student8', 'stu008', 'MCA',   2024-2026', null, NOW(), 'student8@erp.com'),
+('student9', 'stu009', 'MCA',   2024-2026', null, NOW(), 'student9@erp.com'),
+('student10', 'stu0010', 'MCA', 2024-2026', null, NOW(), 'student10@erp.com')
 
+
+'''
